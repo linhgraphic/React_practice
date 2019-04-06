f45e8421029a24c09f64a49a37f7a7c437e70cc2
Linh <linhgraphic@gmail.com>
2019-04-06 18:15:46 +0100

    Updates

--- src/components/button.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-const Button = props => (
-  <button className={props.className} onClick={props.onClick}>
-    {props.children}
-  </button>
-);
-export default Button;
--- src/components/list.jsx
+++
@@ -1,4 +0,0 @@
-let fizzBuzz = i =>
-  i % 15 === 0 ? "fizzBuzz" : i % 3 === 0 ? "fizz" : i % 5 === 0 ? "buzz" : i;
-export const numToArr = num =>
-  Array.from({ length: num }, (_, i) => fizzBuzz(i + 1));
